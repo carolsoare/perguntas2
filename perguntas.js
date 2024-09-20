c41198b9ef73265793f15fe47f7496c4fc442800
carolsoare <165330020+carolsoare@users.noreply.github.com>
2024-09-20 11:44:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'scooby-doo',
+    'Qual o nome da equipe do Scooby',
+    'Daphne; Salsicha; velma, Fred; Scooby-Doo'
+)
+
+criaCartao(
+    'scooby-doo',
+    'Como é o nome do dono do Scooby-Doo?',
+    'Scoobert Cornelius Doo'
+)
+
+criaCartao(
+    'scooby-doo',
+    'Quantos anos tem Scooby-Doo?',
+    'Há 50 anos,'
+)
+
+criaCartao(
+    'scooby-doo',
+    'O que o Scooby-Doo gosta de comer?',
+    ' biscoitos de caramelo'
+)
